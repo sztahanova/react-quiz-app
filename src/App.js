c3049bf83a8c8f18beeeb2d26307adce0b602035
sztahanova <sztahanova@github.com>
2021-01-31 13:15:05 +0100

    Implemented basic quiz behaviour

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import React, { useState } from 'react';
 import questions from './questions';
 
 export default function App() {
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+
+	const handleAnswerButtonClick = () => {
+		const nextQuestion = currentQuestion + 1;
+
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			alert('you reached the end of the quiz');
+		}
+	}
+
 	return (
 		<div className='app'>
 			{/* HINT: replace "false" with logic to display the score when the user has answered all the questions */}
@@ -11,15 +23,14 @@ export default function App() {
 					<>
 						<div className='question-section'>
 							<div className='question-count'>
-								<span>Question 1</span>/{questions.length}
+								<span>Question {currentQuestion + 1}</span>/{questions.length}
 							</div>
-							<div className='question-text'>This is where the question text should go</div>
+							<div className='question-text'>{questions[currentQuestion].questionText}</div>
 						</div>
 						<div className='answer-section'>
-							<button>Answer 1</button>
-							<button>Answer 2</button>
-							<button>Answer 3</button>
-							<button>Answer 4</button>
+							{questions[currentQuestion].answerOptions.map((answer) => {
+								return <button onClick={handleAnswerButtonClick}>{answer.answerText}</button>
+							})}
 						</div>
 					</>
 				)}
