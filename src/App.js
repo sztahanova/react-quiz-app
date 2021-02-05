804fa03c408d1fdaca9eac21199303168a282d9a
sztahanova <sztahanova@github.com>
2021-02-05 22:27:46 +0100

    Added prettier

--- src/App.js
+++ src/App.js
@@ -1,44 +1,54 @@
-import React, { useState } from 'react';
-import questions from './questions';
+import React, { useState } from "react";
+import questions from "./questions";
 
 export default function App() {
-	const [currentQuestion, setCurrentQuestion] = useState(0);
-	const [showScore, setShowScore] = useState(false);
-	const [score, setScore] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
 
-	const handleAnswerButtonClick = (isCorrect) => {
-		const nextQuestion = currentQuestion + 1;
-		
-		if (isCorrect) {
-			setScore(score + 1);
-		}
+  const handleAnswerButtonClick = (isCorrect) => {
+    const nextQuestion = currentQuestion + 1;
 
-		if (nextQuestion < questions.length) {
-			setCurrentQuestion(nextQuestion);
-		} else {
-			setShowScore(true);
-		}
-	}
+    if (isCorrect) {
+      setScore(score + 1);
+    }
 
-	return (
-		<div className='app'>
-			{showScore ? (
-				<div className='score-section'>You scored {score} out of {questions.length}</div>
-			) : (
-					<>
-						<div className='question-section'>
-							<div className='question-count'>
-								<span>Question {currentQuestion + 1}</span>/{questions.length}
-							</div>
-							<div className='question-text'>{questions[currentQuestion].questionText}</div>
-						</div>
-						<div className='answer-section'>
-							{questions[currentQuestion].answerOptions.map((answer) => {
-								return <button onClick={() => handleAnswerButtonClick(answer.isCorrect)}>{answer.answerText}</button>
-							})}
-						</div>
-					</>
-				)}
-		</div>
-	);
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
+  return (
+    <div className="app">
+      {showScore ? (
+        <div className="score-section">
+          You scored {score} out of {questions.length}
+        </div>
+      ) : (
+        <>
+          <div className="question-section">
+            <div className="question-count">
+              <span>Question {currentQuestion + 1}</span>/{questions.length}
+            </div>
+            <div className="question-text">
+              {questions[currentQuestion].questionText}
+            </div>
+          </div>
+          <div className="answer-section">
+            {questions[currentQuestion].answerOptions.map((answer) => {
+              return (
+                <button
+                  onClick={() => handleAnswerButtonClick(answer.isCorrect)}
+                >
+                  {answer.answerText}
+                </button>
+              );
+            })}
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
